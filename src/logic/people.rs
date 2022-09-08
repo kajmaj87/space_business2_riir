83a28a314128741a8f56b9664de51edb39396209
kajman <kajmaj87@gmail.com>
2022-09-08 06:11:24 +0200

    Added move system

--- src/logic/people.rs
+++ src/logic/people.rs
@@ -16,6 +16,12 @@ pub struct Person;
 #[derive(Component)]
 pub struct Dead;
 
+#[derive(Component)]
+pub struct Move {
+    pub dx: f32,
+    pub dy: f32,
+}
+
 // Position and GridPostion are already defined in bevy::prelude
 #[derive(Component)]
 pub struct GridCoords {
@@ -40,6 +46,7 @@ impl Plugin for PeoplePlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(init_people)
             .add_system(hunger_system)
+            .add_system(move_system)
             .add_system(cleanup_system);
     }
 }
@@ -51,7 +58,7 @@ pub fn init_people(mut commands: Commands, asset_server: Res<AssetServer>) {
         type_marker: Person,
         hunger: Hunger(0.0),
         food: FoodAmount(3),
-        position: GridCoords { x: 0.0, y: 0.0 },
+        position: GridCoords { x: 5.0, y: 3.0 },
         sprite: SpriteBundle {
             texture: asset_server.load("person.png"),
             transform: Transform {
@@ -80,6 +87,26 @@ fn hunger_system(
                 .entity(person)
                 .insert(Ttl(config.game.person_ttl.value));
             info!("Person hunger value: {}, person has died", hunger.0);
+        } else {
+            commands.entity(person).insert(Move { dx: -0.1, dy: -0.2 });
+        }
+    }
+}
+
+fn move_system(
+    mut commands: Commands,
+    mut query: Query<(Entity, &Move, &mut GridCoords)>,
+    config: Res<Config>,
+) {
+    for (person, move_component, mut coords) in query.iter_mut() {
+        commands.entity(person).remove::<Move>();
+        let newx = move_component.dx + coords.x;
+        let newy = move_component.dy + coords.y;
+        if 0.0 <= newx && newx <= config.map.size_x.value as f32 - 1.0 {
+            coords.x = newx;
+        }
+        if 0.0 <= newy && newy <= config.map.size_y.value as f32 - 1.0 {
+            coords.y = newy;
         }
     }
 }
