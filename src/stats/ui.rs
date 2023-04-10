e1201ea282a93a46f4f4bdd4aaeb3a3b588a381c
kajman <kajmaj87@gmail.com>
2023-04-10 10:19:51 +0200

    Added current food values in stats window

--- src/stats/ui.rs
+++ src/stats/ui.rs
@@ -7,5 +7,7 @@ pub fn stats_window(mut egui_context: EguiContexts, stats: Res<Statistics>) {
     egui::Window::new("Stats").show(egui_context.ctx_mut(), |ui| {
         ui.label(format!("Current people: {}", stats.current_people));
         ui.label(format!("Current food: {}", stats.current_food));
+        ui.label(format!("Current apples: {}", stats.current_apples));
+        ui.label(format!("Current oranges: {}", stats.current_oranges));
     });
 }
